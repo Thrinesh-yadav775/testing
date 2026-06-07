b88a6860d131d8a45c8ddcdccdd6e4d57f055787
Thrinesh-Yadav-775 <thrineshyadav476@gmail.com>
2026-06-07 17:10:59 +0530

    new dashboard added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-
-
 export default function Home() {
   return (
-  <div>this is dashboard</div>
+    <div>
+      <h1>this is dashbaord</h1>
+      <h2>this is also new dashboard</h2>
+    </div>
   );
 }
